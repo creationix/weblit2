190ce2a37f9f138fe06250ba0ee35114cbfd5201
Tim Caswell <tim@creationix.com>
2019-11-21 16:06:35 -0600

    Add more zip examples

--- src/test-zip.ts
+++ src/test-zip.ts
@@ -1,12 +1,16 @@
 import { readFileStream } from "./fs.js";
 import { p } from "./pretty-print.js";
-import { consume } from "./utils.js";
+import { consume, utf8Decode } from "./utils.js";
 import { Reader } from "./zip.js";
 
 consume(readFileStream("zip.zip")).then((data) => {
     const reader = new Reader(data);
     reader.forEach((entry) => {
         p(entry);
+        const contents = utf8Decode(entry.getData());
+        p({ contents });
     });
     reader.iterator();
+    p(reader.toObject());
+    p(reader.toObject("utf8"));
 });
